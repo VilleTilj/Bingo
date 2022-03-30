a6423a4ce04b1948bb84d0efe6f2c17d1bc32658
Tiljander Ville <ville.tiljander@tuni.fi>
2022-03-30 19:02:49 +0300

    Add key bindings and correct placement for the grid.

--- Bingo/bingo.py
+++ Bingo/bingo.py
@@ -3,9 +3,14 @@ This software is made as a visualization tool for Bingo host to show what number
 '''
 
 import tkinter as tk
-import numpy as np
+
 from tkinter import Menu
 from tkinter import font as tkFont
+
+GRID_X_START = 480
+GRID_Y_START = 20
+GRID_Y_DELTA = 66
+GRID_X_DELTA = 75
 
 '''
 Class Bingo_board is responsible for drawing the buttons with numbers on the tkinter mainwindow. 
@@ -18,7 +23,9 @@ class Bingo_board:
         self.__create_menu()
         self.buttons: list = []
         self.__create_buttons()
-        
+        self.is_fullscreen = False
+
+        self.mainwindow.bind("<Escape>", lambda x: root.destroy())
 
     def __create_buttons(self):
         '''
@@ -36,15 +43,15 @@ class Bingo_board:
             for _ in range(10):
                 new_button = tk.Button(self.mainwindow, text=str(bingo_index), bg='white', height = 2, 
                                          width = 5, font=font, command=lambda index=bingo_index - 1: self.__change_button_color(index))
-                new_button.place(x= 150 + x_delta, y= 40 + y_delta)
+                new_button.place(x= GRID_X_START + x_delta, y= GRID_Y_START + y_delta)
                 self.buttons.append(new_button)
                 
                 # Change row and adjust parameters for next button placement.
                 if bingo_index % 10 == 0:
                     x_delta = 0
-                    y_delta += 65
+                    y_delta += GRID_Y_DELTA
                 else:
-                    x_delta += 75
+                    x_delta += GRID_X_DELTA
                 bingo_index += 1
 
 
@@ -57,6 +64,7 @@ class Bingo_board:
         self.mainwindow.config(menu=menu_bar)
         Menu_toolbar = Menu(menu_bar)
         Menu_toolbar.add_command(label="Exit software", command=self.__destroy_window)
+        Menu_toolbar.add_command(label='Full screen on/off', command=self.toggle_fullscreen)
         menu_bar.add_cascade(label="Options", menu=Menu_toolbar)
 
 
@@ -66,7 +74,20 @@ class Bingo_board:
         Changes the bingo sheet's buttons color when pressed.
         @ index : Index to find out wanted button
         '''
-        self.buttons[index].configure(bg='red') if self.buttons[index]['bg'] == 'white' else self.buttons[index].configure(bg='white')
+        self.buttons[index].configure(bg='#10f520') if self.buttons[index]['bg'] == 'white' else self.buttons[index].configure(bg='white')
+
+
+    def toggle_fullscreen(self):
+        '''
+        Private method:
+        Change between fullscreen and non fullscreen.
+        '''
+        if self.is_fullscreen:
+            root.attributes("-fullscreen", False)
+            self.is_fullscreen = False
+        else:
+            root.attributes("-fullscreen", True)  
+            self.is_fullscreen = True
 
 
     def __destroy_window(self):
@@ -77,10 +98,14 @@ class Bingo_board:
 if __name__ == '__main__':
     # Init mainwindow
     root = tk.Tk()
-    root.attributes("-fullscreen", True)  
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()   
+    root.geometry(f'{screen_width}x{screen_height}')
+    root.attributes("-fullscreen", False)  
 
     root.title("Bingo Screen")
 
     app = Bingo_board(root)
 
+    # Start game loop
     root.mainloop()
